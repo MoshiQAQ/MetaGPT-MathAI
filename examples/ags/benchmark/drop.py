a7619dee4915c5e332828c3479696f5b22675cc8
Zhaoyang Yu <yuzhaoyang0713@ruc.edu.cn>
2024-09-19 17:11:58 +0800

    Update drop.py

--- examples/ags/benchmark/drop.py
+++ examples/ags/benchmark/drop.py
@@ -24,18 +24,6 @@ def _white_space_fix(text: str) -> str:
 
 EXCLUDE = set(string.punctuation)
 
-def _is_number(text: str) -> bool:
-    try:
-        float(text)
-        return True
-    except ValueError:
-        return False
-
-def _normalize_number(text: str) -> str:
-    if _is_number(text):
-        return str(float(text))
-    else:
-        return text
 
 def _remove_punc(text: str) -> str:
     if not _is_number(text):
@@ -62,6 +50,21 @@ def _normalize_answer(text: str) -> str:
     parts = [part for part in parts if part.strip()]
     normalized = " ".join(parts).strip()
     return normalized
+
+
+def _is_number(text: str) -> bool:
+    try:
+        float(text)
+        return True
+    except ValueError:
+        return False
+
+
+def _normalize_number(text: str) -> str:
+    if _is_number(text):
+        return str(float(text))
+    else:
+        return text
 
 
 def _answer_to_bags(
@@ -115,6 +118,7 @@ def _compute_f1(predicted_bag: Set[str], gold_bag: Set[str]) -> float:
     )
     return f1
 
+
 def _match_numbers_if_present(gold_bag: Set[str], predicted_bag: Set[str]) -> bool:
     gold_numbers = set()
     predicted_numbers = set()
@@ -128,41 +132,8 @@ def _match_numbers_if_present(gold_bag: Set[str], predicted_bag: Set[str]) -> bo
         return True
     return False
 
-def _compute_f1(predicted_bag: Set[str], gold_bag: Set[str]) -> float:
-    intersection = len(gold_bag.intersection(predicted_bag))
-    if not predicted_bag:
-        precision = 1.0
-    else:
-        precision = intersection / float(len(predicted_bag))
-    if not gold_bag:
-        recall = 1.0
-    else:
-        recall = intersection / float(len(gold_bag))
-    f1 = (
-        (2 * precision * recall) / (precision + recall)
-        if not (precision == 0.0 and recall == 0.0)
-        else 0.0
-    )
-    return f1
 
-def _align_bags(predicted: List[Set[str]], gold: List[Set[str]]) -> List[float]:
-    """
-    Takes gold and predicted answer sets and first finds the optimal 1-1 alignment
-    between them and gets maximum metric values over all the answers.
-    """
-    scores = np.zeros([len(gold), len(predicted)])
-    for gold_index, gold_item in enumerate(gold):
-        for pred_index, pred_item in enumerate(predicted):
-            if _match_numbers_if_present(gold_item, pred_item):
-                scores[gold_index, pred_index] = _compute_f1(pred_item, gold_item)
-    row_ind, col_ind = linear_sum_assignment(-scores)
-
-    max_scores = np.zeros([max(len(gold), len(predicted))])
-    for row, column in zip(row_ind, col_ind):
-        max_scores[row] = max(max_scores[row], scores[row, column])
-    return max_scores
-
-def get_metrics(
+def get_drop_metrics(
     predicted: Union[str, List[str], Tuple[str, ...]], gold: Union[str, List[str], Tuple[str, ...]]
 ) -> Tuple[float, float]:
     """
@@ -182,8 +153,9 @@ def get_metrics(
 
     f1_per_bag = _align_bags(predicted_bags[1], gold_bags[1])
     f1 = np.mean(f1_per_bag)
-    f1 = round(f1, 2)
+
     return exact_match, f1
+
 
 def answer_json_to_strings(answer: Dict[str, Any]) -> Tuple[Tuple[str, ...], str]:
     """
@@ -200,7 +172,7 @@ def answer_json_to_strings(answer: Dict[str, Any]) -> Tuple[Tuple[str, ...], str
                 [
                     "{0} {1} {2}".format(
                         answer["date"]["day"], answer["date"]["month"], answer["date"]["year"]
-                    )
+                    ).strip()
                 ]
             ),
             "date",
@@ -210,16 +182,42 @@ def answer_json_to_strings(answer: Dict[str, Any]) -> Tuple[Tuple[str, ...], str
             f"Answer type not found, should be one of number, spans or date at: {json.dumps(answer)}"
         )
 
-def load_data(file_path: str, samples: int, test=False) -> List[Tuple[str, Dict[str, Any]]]:
-    with open(file_path, mode="r") as file:
-        data = json.load(file)
-        data = list(data.items())
 
-    random_indices = generate_random_indices(len(data), samples, test)
-    data = [data[i] for i in random_indices]
-    return data
+def answer_json_to_string(answer_json):
+    return json.dumps(answer_json_to_strings(answer_json))
 
-async def evaluate_problem(question: str, passage: str, answers: List[Dict[str, Any]], graph: Callable) -> Tuple[str, str, float]:
+
+def normalize(s: str) -> str:
+    """Lower text and remove punctuation, articles and extra whitespace."""
+    s = s.lower()
+    exclude = set(string.punctuation)
+    s = "".join(char for char in s if char not in exclude)
+    s = re.sub(r"\b(a|an|the)\b", " ", s)
+    s = " ".join(s.split())
+    return s
+
+
+def fuzzy_match(s1: str, s2: str) -> bool:
+    s1 = normalize(s1)
+    s2 = normalize(s2)
+
+    if s1 == "" or s2 == "":
+        return s1 == s2
+
+    return s1 in s2 or s2 in s1
+
+
+def drop_metric(sample: str, reference: list[str]) -> Tuple[float, float]:
+    em_scores = []
+    f1_scores = []
+    for answer in reference:
+        if answer.strip() != "":
+            em, f1 = get_drop_metrics(sample, answer)
+            em_scores.append(em)
+            f1_scores.append(f1)
+    return (max(em_scores), max(f1_scores))
+
+async def evaluate_problem(inputs: str, answers: List[Dict[str, Any]], graph: Callable) -> Tuple[str, str, float]:
 
     max_retries = 5
     retries = 0
@@ -227,21 +225,27 @@ async def evaluate_problem(question: str, passage: str, answers: List[Dict[str, 
     while retries < max_retries:
         try:
             global cost
-            prediction, cost = await graph(question, passage)
+            prediction, cost = await graph(inputs)
 
-
-            max_score = 0.0
-            max_type = None
+            f1_scores = []
             best_answer = None
 
             for answer in answers:
-                golden_answer, golden_type = answer_json_to_strings(answer)
-                _, f1_score = get_metrics(prediction, golden_answer)
-                if golden_answer[0].strip() != "":
-                    max_score = max(max_score, f1_score)
-                    if max_score == f1_score:
-                        max_type = golden_type
-                        best_answer = golden_answer
+                if answer.strip() != "":
+                    _, f1_score = get_drop_metrics(prediction, answer)
+                    f1_scores.append(f1_score)
+
+            max_score = max(f1_scores)
+
+            # matches = [
+            #         fuzzy_match(prediction, answer)
+            #         for answer in answers
+            # ]
+
+            # score = True in matches
+
+            score = max_score
+
             break
 
         except Exception as e:
@@ -250,35 +254,30 @@ async def evaluate_problem(question: str, passage: str, answers: List[Dict[str, 
 
             if retries == max_retries:
                 print("Maximum retries reached. Skipping this sample.")
-                best_answer = None
                 prediction = None
-                max_score = 0.0
+                score = 0.0
                 break
 
-    return best_answer, prediction, max_score
+    return prediction, score
 
-async def evaluate_all_passages(annotations: List[Tuple[str, Dict[str, Any]]], graph: Callable, max_concurrent_tasks: int = 50) -> List[List[Any]]:
+async def evaluate_all_questions(annotations: List[Tuple[str, Dict[str, Any]]], graph: Callable, max_concurrent_tasks: int = 50) -> List[List[Any]]:
     semaphore = asyncio.Semaphore(max_concurrent_tasks)
     results = []
 
-    async def sem_evaluate(id: str, annotation: Dict[str, Any]):
+    async def sem_evaluate(annotation: Dict[str, Any]):
         async with semaphore:
-            passage = annotation["passage"]
-            for qa_pair in annotation["qa_pairs"]:
-                question = qa_pair["question"]
-                answers = [qa_pair["answer"]]
-                if "validated_answers" in qa_pair and qa_pair["validated_answers"]:
-                    answers += qa_pair["validated_answers"]
-                best_answer, prediction, score = await evaluate_problem(question, passage, answers, graph)
-                results.append([id, question, prediction, best_answer, score])
+            inputs = annotation["context"]
+            answers = annotation["targets"]
+            prediction, score = await evaluate_problem(inputs, answers, graph)
+            results.append([annotation["id"], prediction, answers, score])
 
-    tasks = [sem_evaluate(id, annotation) for id, annotation in annotations]
+    tasks = [sem_evaluate(annotation) for annotation in annotations]
     await tqdm_asyncio.gather(*tasks, desc="Evaluating DROP passages", total=len(annotations))
 
     return results
 
 def save_results_to_csv(results: List[List[Any]], path: str) -> float:
-    df = pd.DataFrame(results, columns=["id", "question", "prediction", "best_answer", "score"])
+    df = pd.DataFrame(results, columns=["id", "prediction", "answers", "score"])
     average_score = df["score"].mean()
 
     output_file = f"{path}/{average_score:.5f}.csv"
@@ -287,11 +286,29 @@ def save_results_to_csv(results: List[List[Any]], path: str) -> float:
 
     return average_score
 
+# -- From ADAS --
+
+def load_drop(file_path, samples, test=False, total_length=928):
+    import gzip
+    with gzip.open(file_path, "rb") as file:
+        data = [json.loads(line) for line in file]
+
+    random_indices = generate_random_indices(len(data), total_length, False)
+    random_indices = random_indices[:samples] if not test else random_indices[samples:]
+    examples = [data[i] for i in random_indices]
+
+    for example in examples:
+        example["targets"] = example["ref_text"].split("|")    
+
+    return examples
+
 async def drop_evaluation(graph: Callable, file_path: str, samples: int, path: str, test=False) -> float:
-    data = load_data(file_path, samples, test=test)
-    results = await evaluate_all_passages(data, graph, max_concurrent_tasks=20)
+    # data = load_data(file_path, samples, test=test)
+    data = load_drop(file_path, samples, test=test)
+    results = await evaluate_all_questions(data, graph, max_concurrent_tasks=8)
     average_score = save_results_to_csv(results, path=path)
     print(f"Average score on DROP dataset: {average_score:.5f}")
     global cost
-    print(f"Total cost: {cost}")
+    print(f"Total cost: {cost: .5f}")
+    print(f"Cost per sample: {(cost / len(data)):.9f}")
     return average_score
